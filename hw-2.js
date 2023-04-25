9b296060eb61cc43e99d4abde6155e5cc7e2bc4e
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-04-25 12:56:03 +0400

    выполнено задание 6

--- hw-2.js
+++ hw-2.js
@@ -1,7 +1,7 @@
-let a = 10;
-alert (a);
-a = 20;
-alert (a); // задание 1
+let A = 10;
+alert (A);
+A = 20;
+alert (A); // задание 1
 
 let year = 2007;
 alert (year); // задание 2
@@ -17,3 +17,6 @@ alert (x / y); // задание 4
 
 alert (2**5); // задание 5
 
+let a = 9;
+let b = 2;
+alert (a % b); // задание 6
