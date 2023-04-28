c406b513b5ae4849b2a9b9d15d3c1b40f076b06c
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-04-28 14:46:15 +0400

    выполнено задание 3

--- hw-3.js
+++ hw-3.js
@@ -16,3 +16,14 @@ if (c > 0 && c < 10) {
 } else {
     console.log ('Неверно');
 }
+
+//Задание 3
+
+let d = Number(2);
+let e = Number(166);
+if (d > 100 || e > 100) {
+    console.log ('Верно');
+} else {
+    console.log ('Неверно');
+}
+
