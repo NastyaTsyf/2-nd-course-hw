aceb585c7e62d942bac3d89057596e1c9fe0ce85
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-04-28 15:30:34 +0400

    выполнено задание 5

--- hw-3.js
+++ hw-3.js
@@ -33,3 +33,59 @@ let a = '2';
 let b = '3';
 // Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
 alert(a =+ b);
+
+//Задание 5
+
+let monthNumber = prompt ('Введите номер месяца');
+switch (monthNumber) {
+    case '1' :
+        alert ('Зима');
+        break;
+    case '2' :
+        alert ('Зима');
+        break;
+
+    case '12' :
+        alert ('Зима');
+        break;
+
+    case '3' :
+        alert ('Весна');
+        break;
+
+    case '4' :
+        alert ('Весна');
+        break;
+
+    case '5' :
+        alert ('Весна');
+        break;
+
+    case '6' :
+        alert ('Лето');
+        break;
+
+    case '7' :
+        alert ('Лето');
+        break;
+
+    case '8' :
+        alert ('Лето');
+        break;
+
+    case '9' :
+        alert ('Осень');
+        break;
+
+    case '10' :
+        alert ('Осень');
+        break;
+
+    case '11' :
+        alert ('Осень');
+        break;
+
+    default:
+        alert ('Неверный ввод');
+        break;
+}
