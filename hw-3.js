cfcfaa47279e98ee7d531063efb297d2fe13d701
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-05-03 18:18:00 +0400

    устранены замечания наставника, выполнены задания 7,8,9

--- hw-3.js
+++ hw-3.js
@@ -10,7 +10,7 @@ if (password == userPassword) {
 
 //Задание 2
 
-let c = Number(2);
+let c = 2;
 if (c > 0 && c < 10) {
     console.log ('Верно');
 } else {
@@ -32,55 +32,32 @@ if (d > 100 || e > 100) {
 let a = '2';
 let b = '3';
 // Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
-alert(a =+ b);
+alert (Number(a) + Number(b));
 
 //Задание 5
 
 let monthNumber = prompt ('Введите номер месяца');
 switch (monthNumber) {
     case '1' :
-        alert ('Зима');
-        break;
     case '2' :
-        alert ('Зима');
-        break;
-
     case '12' :
         alert ('Зима');
         break;
 
     case '3' :
-        alert ('Весна');
-        break;
-
     case '4' :
-        alert ('Весна');
-        break;
-
     case '5' :
         alert ('Весна');
         break;
 
     case '6' :
-        alert ('Лето');
-        break;
-
     case '7' :
-        alert ('Лето');
-        break;
-
     case '8' :
         alert ('Лето');
         break;
 
     case '9' :
-        alert ('Осень');
-        break;
-
     case '10' :
-        alert ('Осень');
-        break;
-
     case '11' :
         alert ('Осень');
         break;
@@ -88,4 +65,36 @@ switch (monthNumber) {
     default:
         alert ('Неверный ввод');
         break;
+}
+
+// Задание 7
+
+let x = prompt ('Пожалуйста, введите любое число');
+if (isNaN(x) === true) {
+	alert('введено не число');
+} else if (x % 2 === 0) {
+	alert('Четное');
+} else {
+	alert('Нечетное');
+}
+
+
+// Задания 8, 9
+
+let clientOS = Number(prompt ('Введите 0, если пользуетесь iOS, 1, если Android'));
+let clientDeviceYear = Number(prompt ('Введите год выпуска Вашего устройства'));
+if (clientOS == 0) {
+    if (clientDeviceYear >= 2015) {
+        alert('Установите версию приложения для iOS по ссылке');
+    } else {
+        alert('Установите облегченную версию приложения для iOS по ссылке');
+    }
+} else if (clientOS === 1) {
+    if (clientDeviceYear >= 2015) {
+        alert('Установите версию приложения для Android по ссылке');
+    } else {
+        alert('Установите облегченную версию приложения для Android по ссылке');
+    }
+} else {
+	alert('Неверный ввод');
 }
