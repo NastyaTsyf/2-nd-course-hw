44253c735d710f2c7b1c0bb6508b2645baf6c28f
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-05-09 17:47:52 +0400

    task 3 completed

--- hw-5.js
+++ hw-5.js
@@ -1,3 +1,4 @@
+"use strict"
 //Задание 1
 
 function chooseMore(a, b) {
@@ -23,3 +24,17 @@ function checkParity(n) {
 
 checkParity(9);
 
+//Задание 3
+//3.1 
+
+const calcSquare = (x) => {
+    console.log (x ** 2);
+};
+calcSquare(6);
+
+//3.2
+
+const returnSquare = (x) => {
+    return x ** 2;
+};
+console.log (returnSquare(5));
