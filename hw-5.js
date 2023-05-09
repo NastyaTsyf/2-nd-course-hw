2ac7655e655d9a8d599367281060667e4d27bbcd
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-05-09 17:32:58 +0400

    task 1 completed

---
+++ hw-5.js
@@ -0,0 +1,12 @@
+//Задание 1
+
+function chooseMore(a, b) {
+    if (a >= b) {
+        console.log (a);
+    } else {
+        console.log (b);
+    }
+}
+
+console.log (chooseMore(5, 8));
+console.log (chooseMore(5, 5));
