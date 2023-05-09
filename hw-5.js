de68a74f23f2bcbb9940a2f83cd6227da8edc907
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-05-09 17:37:54 +0400

    task 2 completed

--- hw-5.js
+++ hw-5.js
@@ -8,5 +8,18 @@ function chooseMore(a, b) {
     }
 }
 
-console.log (chooseMore(5, 8));
-console.log (chooseMore(5, 5));
+chooseMore(5, 8);
+chooseMore(5, 5);
+
+//Задание 2
+
+function checkParity(n) {
+    if (n % 2 == 0) {
+        console.log ('Число четное');
+    } else {
+        console.log ('Число нечетное');
+    }
+}
+
+checkParity(9);
+
