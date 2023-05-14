e7564da917f8ee2c53b54f6f7c5abce3052f4e7a
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-05-14 18:37:24 +0400

    task 3 completed

--- hw-6.js
+++ hw-6.js
@@ -15,7 +15,12 @@ let array = [1, 5, 4, 10, 0, 3];
 
 array.forEach((el, index) => {
     if (el == 4) {
-        console.log(`Поиция числа 4 - ${index}`);
+        console.log(`Позция числа 4 - ${index}`);
     }
 });
 
+// Задание 3
+
+let arr = [1, 3, 5, 10, 20];
+arr = arr.join([' ']);
+console.log(arr);
