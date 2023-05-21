ac39bc510ab1df0998307946a686525e8e95e744
NastyaTsyf <nastya-cyfarkina@mail.ru>
2023-05-21 19:17:51 +0400

    task 3 completed

--- hw-7.js
+++ hw-7.js
@@ -4,3 +4,16 @@ let jsStr = 'js';
 jsStr = jsStr.toUpperCase();
 console.log(jsStr);
 
+//Задание 2
+
+
+
+//Задание 3
+
+let num = 32.58884;
+less = Math.floor(num);
+console.log(less);
+more = Math.ceil(num);
+console.log(more);
+whole = Math.round(num);
+console.log(whole);
